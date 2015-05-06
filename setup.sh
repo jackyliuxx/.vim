eb4ec3bf4da1f08005b1726f0be8445c477e3e73
jackyliuxx <jackyliuxx@gmail.com>
2015-05-06 18:38:06 +0800

    dododo

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ -e "/usr/bin/git" ];then
+
+else
+    sudo apt-get -y install git
+fi
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/powerline/fonts.git ./powerlinefont
+./powerlinefont/install.sh
+
+if [ -e "$HOME/.vimrc" ];then
+    mv ~/.vimrc ~/.vimrc.backup
+fi
+cp ./vimrc ~/.vimrc
