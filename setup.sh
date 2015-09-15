404f641f9e44b02fd935a521b1ba173f2ad205ec
jackyliuxx <jackyliuxx@jackyliuxx.com>
2015-09-15 12:19:54 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if [ -e "/usr/bin/git" ];then
-    echo 'found git!'
-else
-    sudo apt-get -y install git
+if [ $(id -u) != 0 ];then
+    echo "Please run as root."
+    exit
 fi
+apt-get -y install git vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/powerline/fonts.git ./powerlinefont
 ./powerlinefont/install.sh
@@ -12,3 +12,7 @@ if [ -e "$HOME/.vimrc" ];then
     mv ~/.vimrc ~/.vimrc.backup
 fi
 cp ./vimrc ~/.vimrc
+vim +PluginInstall +qall
+apt-get install build-essential cmake python-dev
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
